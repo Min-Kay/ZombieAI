4a97050355b5a8588f0fd19cfd1c2d3190a9ffa4
MinKay <chris1410@naver.com>
2021-02-22 22:30:49 +0900

    경로탐색 수정 및 충돌 반발 수정

--- Assets/Scripts/ZombieController.cs
+++ Assets/Scripts/ZombieController.cs
@@ -13,6 +13,7 @@ public class ZombieController : MonoBehaviour
     
     private Animator animator;
     private NavMeshAgent agent;
+    [Header("Ray Position")]
     public Transform rayPos;
 
     private WaitForSeconds ws;
@@ -24,26 +25,28 @@ public class ZombieController : MonoBehaviour
     public enum State { IDLE,  PATROL, CHASING, ATTACK, DIE};
     public enum Mode { Basic, Spread}
 
+    [Header("Zombie State & Move Logic")]
     public State state = State.IDLE;
     public Mode mode = Mode.Basic;
 
     private bool isDie = false;
 
-    public float attackDist = 5.0f;
+    [Header("Zombie Info")]
+    public float attackDist = 1.5f;
+    public float spreadDist = 3.0f;
     public float patrolRadius = 15.0f;
     public float callZombies = 10.0f;
     public float patrolSpeed = 1.5f;
     public float chaseSpeed = 5.0f;
     public float damping = 1.0f;
+    public float speed
+    {
+        get { return agent.velocity.magnitude; }
+    }
 
     private float attackTime;
     private float idleTime;
     private float dieTime;
-
-    public float speed 
-    {
-        get { return agent.velocity.magnitude; }
-    }
 
      void Awake()
     {
@@ -57,6 +60,7 @@ public class ZombieController : MonoBehaviour
 
         agent.updateRotation = false;
         agent.autoBraking = false;
+        agent.avoidancePriority = Random.Range(0, 10);
     }
 
     void OnEnable()
@@ -66,34 +70,44 @@ public class ZombieController : MonoBehaviour
 
     void FixedUpdate()
     {
-        animator.SetFloat("Speed", speed);
-
-        if(state != State.ATTACK)
+        if(!isDie)
         {
+            animator.SetFloat("Speed", speed);
             View();
-        }
+            if(state != State.IDLE)
+            {
+                Quaternion rot = Quaternion.LookRotation(state != State.ATTACK ? agent.desiredVelocity : (targetPos - transform.position).normalized);
+                transform.rotation = Quaternion.Slerp(transform.rotation, rot, Time.deltaTime * damping);
+            }
+            damping = ((state != State.ATTACK && state != State.CHASING) ? 1.0f : 7.0f);
 
-        if(agent.isStopped == false)
-        {
-            Quaternion rot = Quaternion.LookRotation(agent.desiredVelocity);
-            transform.rotation = Quaternion.Slerp(transform.rotation, rot, Time.deltaTime * damping);
+            if (Vector3.Distance(targetPos, transform.position) <= attackDist && state == State.CHASING)
+            {
+                state = State.ATTACK;
+            }
+            else if (Vector3.Distance(targetPos, transform.position) > attackDist && state == State.ATTACK)
+                state = State.CHASING;
         }
     }
 
-    private void NearZombieAttack(Vector3 pos)
+    private void NearZombieAttack()
     {
         GameObject[] zombies = GameObject.FindGameObjectsWithTag("Zombie");
 
         foreach(var i in zombies)
         {
             if(Vector3.Distance(i.transform.position,transform.position) <= callZombies)
+            {
+                i.GetComponent<ZombieController>().SetTarget(target);
                 i.SendMessage("Detect", SendMessageOptions.DontRequireReceiver);
+            }
         }     
     }
 
     private void Detect()
     {
-        state = State.CHASING;
+        if(state != State.ATTACK && state != State.CHASING)
+            state = State.CHASING;
     }
 
     private Vector3 BoundaryAngle(float angle) //시야각 계산
@@ -110,17 +124,9 @@ public class ZombieController : MonoBehaviour
         Debug.DrawRay(rayPos.position, left * viewDistance, Color.red);
         Debug.DrawRay(rayPos.position, right * viewDistance, Color.red);
 
-        targetPos.x = target.position.x;
-        targetPos.y = transform.position.y;
-        targetPos.z = target.position.z;
-
-        randomPos.x = target.position.x + (Random.insideUnitCircle.normalized.x * attackDist);
-        randomPos.y = transform.position.y;
-        randomPos.z = target.position.z + (Random.insideUnitCircle.normalized.y * attackDist);
-
+        SetLocation();
         Vector3 direction = (targetPos - transform.position).normalized;
-        Vector3 directPos = (randomPos - transform.position).normalized;
-
+        
         float angle = Vector3.Angle(direction, transform.forward);
 
         if (angle < fieldOfViewAngle * 0.5f)
@@ -130,15 +136,16 @@ public class ZombieController : MonoBehaviour
             {
                 if(hit.transform.gameObject.tag == "Player")
                 {
-                    direction = (hit.transform.position - transform.position).normalized;
-
-                    Debug.DrawRay(rayPos.position, direction * viewDistance, Color.blue);
-                    Debug.DrawRay(rayPos.position, directPos * viewDistance, Color.green);
-
-                    if(state != State.CHASING && state != State.ATTACK)
+                    target = hit.transform;
+                    SetLocation();
+                    direction = (targetPos - transform.position).normalized;
+                    Debug.DrawRay(rayPos.position, direction * Vector3.Distance(targetPos,transform.position), Color.blue);
+                    if(mode == Mode.Spread && randomPos != null)
                     {
-                        state = State.CHASING;
+                        Vector3 directPos = (randomPos - transform.position).normalized;
+                        Debug.DrawRay(rayPos.position, directPos * Vector3.Distance(randomPos, transform.position), Color.green);
                     }
+                    Detect();
                 }
             }
         }
@@ -164,9 +171,9 @@ public class ZombieController : MonoBehaviour
     {
         Vector3 _point;
 
-        if (RandomPoint(transform.position, radius, out _point))
+        if (RandomPoint(point.position, radius, out _point))
         {
-            Debug.DrawRay(_point, Vector3.up, Color.black, 1);
+            Debug.DrawRay(_point, Vector3.up, Color.black, 3);
             return _point;
         }
         return Vector3.zero;
@@ -183,7 +190,7 @@ public class ZombieController : MonoBehaviour
                 case State.DIE:
                     agent.isStopped = true;
                     animator.SetBool("Move", false);
-                    animator.SetBool("Die", true);
+                    animator.SetTrigger("Die");
                     isDie = true;
                     break;
                 case State.IDLE:
@@ -193,51 +200,49 @@ public class ZombieController : MonoBehaviour
                     break;
                 case State.PATROL:
                     agent.isStopped = false;
-                    damping = 1.0f;
-                    if (!agent.hasPath)
+                    animator.SetBool("Move", true);
+                    agent.speed = patrolSpeed;
+                    if (!agent.hasPath || Vector3.Distance(agent.pathEndPosition, transform.position) <= 0.1f)
                     {
-                        agent.speed = patrolSpeed;
-                        animator.SetBool("Move", true);
-                        agent.SetDestination(GetRandomPoint(transform,patrolRadius)); 
-                    }
-                    else if(Vector3.Distance(agent.pathEndPosition,transform.position) < 0.1f)
-                    {
-                        agent.speed = patrolSpeed;
-                        animator.SetBool("Move", true);
-                        agent.SetDestination(GetRandomPoint(transform, patrolRadius));
+                        agent.SetDestination(GetRandomPoint(transform,patrolRadius));
                         RandomState();
                     }
-                    yield return ws;
                     break;
                 case State.CHASING:
                     agent.isStopped = false;
-                    damping = 7.0f;
-                    NearZombieAttack(transform.position);
+                    NearZombieAttack();
                     animator.SetBool("Move", true);
                     agent.speed = chaseSpeed;
-                    if (Vector3.Distance(targetPos, transform.position) <= attackDist)
+                    if (mode == Mode.Spread)
                     {
-                        state = State.ATTACK;
-                        continue;
+                        if (randomPos == null || Vector3.Distance(targetPos, randomPos) > spreadDist)
+                            randomPos = GetRandomPoint(target, spreadDist);
+                        else if(agent.remainingDistance < 0.8f)
+                            randomPos = GetRandomPoint(target, spreadDist+1.0f);
                     }
-                    else
-                        agent.SetDestination(randomPos); 
+                    agent.SetDestination(mode == Mode.Basic || Vector3.Distance(targetPos,transform.position) <= spreadDist?targetPos:randomPos);
                     break;
                 case State.ATTACK:
                     agent.isStopped = true;
-                    NearZombieAttack(transform.position);
                     animator.SetBool("Move", false);
                     animator.SetTrigger("Attack");
-                    if (Vector3.Distance(targetPos, transform.position) > attackDist)
-                    {
-                        state = State.CHASING;
-                        continue;
-                    }
+                    NearZombieAttack();
+                    yield return new WaitForSeconds(attackTime);
                     break;
                 default:
                     break;
             }
         }
+    }
+
+    private void SetLocation()
+    {
+        targetPos = new Vector3(target.position.x, transform.position.y, target.position.z);
+    }
+
+    public void SetTarget(Transform tr)
+    {
+        target = tr;
     }
 
     private void RandomState()
@@ -247,11 +252,9 @@ public class ZombieController : MonoBehaviour
         switch (dice)
         {
             case 0:
-                //agent.SetDestination(transform.position);
                 state = State.IDLE;
                 break;
             default:
-                animator.SetBool("Move", true);
                 state = State.PATROL;
                 break;
         }
@@ -278,7 +281,6 @@ public class ZombieController : MonoBehaviour
     }
 
 #if UNITY_EDITOR
-
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(transform.position, patrolRadius);
